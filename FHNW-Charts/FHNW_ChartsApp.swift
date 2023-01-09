8954974e4cfed89c5ee1ea33384001609a68846f
Boehme, Ingo <ingo.boehme@zuehlke.com>
2023-01-09 16:05:13 +0100

    Initial commit

---
+++ FHNW-Charts/FHNW_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FHNW_ChartsApp.swift
+//  FHNW-Charts
+//
+//  Created by Ingo Boehme on 09.01.23.
+//
+
+import SwiftUI
+
+@main
+struct FHNW_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FHNW-Charts/secrez.swift
@@ -0,0 +1,12 @@
+//
+//  secret.swift
+//  FHNW-Charts
+//
+//  Created by Ingo Boehme on 09.01.23.
+//
+
+import Foundation
+
+class secrets {
+    static let apiKey = "[ add your private API key here]"
+}
